ee6a7c09e0b99ce8409959ce0e1f1eba8ea5e758
ma9da <dodymasry25@gmail.com>
2022-03-23 16:12:06 +0200

    added: counter with useState Hook

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
+import { useState } from "react";
+
 function App() {
+  // gets fired on rerendring
+  const [count, setCount] = useState(0);
+  const [{ count1, count2 }, setCounts] = useState({ count1: 10, count2: 15 })
   return (
-    <div className="App">
-    </div>
+    <>
+      <button onClick={() => { setCount((currentCount) => currentCount + 1) }}>increament</button>
+      <p>{count}</p>
+      <p>count 1: {count1}</p>
+      <button onClick={() => {
+        setCounts(currentState => ({
+          ...currentState,
+          count1: currentState.count1 + 10
+        }
+        ))
+      }}
+      >
+        increment count 1
+      </button>
+      <p>count 2: {count2}</p>
+      <button onClick={() => {
+        setCounts((currentState) => ({
+          ...currentState,
+          count2: currentState.count2 + 15
+        }
+        ))
+      }}
+      >
+      increment count 2
+    </button>
+    </>
   );
 }
 
