9d3273a7fefd748763f77f49c6cef9e74e9de13a
ma9da <dodymasry25@gmail.com>
2022-03-24 18:00:47 +0200

    useCallback hook

--- src/Second.js
+++ src/Second.js
@@ -1,26 +1,26 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import Hello from './Hello';
 import useFetch from './useFetch';
 import { useMemo } from 'react';
-const computeLongestWord = (quote) => {
-    if (!quote) {
-        return []
-    }
-    console.log("computing longest word");
-    let longestWord = ""
-    JSON.parse(quote).forEach(sentence =>
-        sentence.split(' ').forEach(word => {
-            if (word.length > longestWord.length) {
-                longestWord = word
-            }
-        })
-    );
-    return longestWord
-}
 const Second = () => {
     const [count, setCount] = useState(0);
     const { data, loading } = useFetch(`https://raw.githubusercontent.com/ajzbc/kanye.rest/master/quotes.json`)
-    const longestWord = useMemo(() => computeLongestWord(data), [data])
+    const computeLongestWord = useCallback((quote) => {
+        if (!quote) {
+            return []
+        }
+        console.log("computing longest word");
+        let longestWord = ""
+        JSON.parse(quote).forEach(sentence =>
+            sentence.split(' ').forEach(word => {
+                if (word.length > longestWord.length) {
+                    longestWord = word
+                }
+            })
+        );
+        return longestWord
+    },[])
+    const longestWord = useMemo(() => computeLongestWord(data), [data, computeLongestWord])
     return (
         <>
             <p>{count}</p>
