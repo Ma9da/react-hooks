fb74044c417f055b2d004aa10f8a7938bfb2781c
ma9da <dodymasry25@gmail.com>
2022-03-25 16:23:41 +0200

    useReducer todos

--- src/Third.js
+++ src/Third.js
@@ -1,26 +1,56 @@
-import { useReducer } from "react";
-const INCREMENT = 'INCREMENT'
-const DECREMENT = 'DECREMENT'
-// useReducer
+import React, { useReducer, useState } from "react";
+
 function reducer(state, action) {
     switch (action.type) {
-        case INCREMENT:
-            return state + 1
-        case DECREMENT:
-            return state - 1
+        case "add-todo":
+            return {
+                todos: [...state.todos, { text: action.text, completed: false }],
+                todoCount: state.todoCount + 1
+            };
+        case "toggle-todo":
+            return {
+                todos: state.todos.map((t, idx) =>
+                    idx === action.idx ? { ...t, completed: !t.completed } : t
+                ),
+                todoCount: state.todoCount
+            };
         default:
-            return state
+            return state;
     }
 }
+
 const Third = () => {
-    const [count, dispatch] = useReducer(reducer, 0)
+    const [{ todos, todoCount }, dispatch] = useReducer(reducer, {
+        todos: [],
+        todoCount: 0
+    });
+    const [text, setText] = useState();
+
     return (
-        <>
-            <button onClick={() => dispatch({ type: INCREMENT })}>+ </button>
-            <span>{count}</span>
-            <button onClick={() => dispatch({ type: DECREMENT })}>- </button>
-        </>
+        <div>
+            <form
+                onSubmit={e => {
+                    e.preventDefault();
+                    dispatch({ type: "add-todo", text });
+                    setText("");
+                }}
+            >
+                <input value={text} onChange={e => setText(e.target.value)} />
+            </form>
+            <div>number of todos: {todoCount}</div>
+            {todos.map((t, idx) => (
+                <p
+                    key={t.text}
+                    onClick={() => dispatch({ type: "toggle-todo", idx })}
+                    style={{
+                        textDecoration: t.completed ? "line-through" : ""
+                    }}
+                >
+                    {t.text}
+                </p>
+            ))}
+        </div>
     );
-}
+};
 
 export default Third;
